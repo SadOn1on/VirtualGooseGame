9ddd90e627c6b4269df4e18403429c376a7ccc47
SadOn1on <svmzh15@gmail.com>
2025-02-17 12:34:11 +0300

    Fixed scopes

--- backend/src/main/java/com/goosegame/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/goosegame/backend/configuration/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
-                        .anyRequest().hasAuthority("SCOPE_test")
+                        .anyRequest().hasAuthority("SCOPE_read")
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults())
